27c39019e809b3f0ae999542f1b1d935d4c081cd
Alexandre Colauto <43041764+AlexandreColauto@users.noreply.github.com>
2022-04-14 15:55:23 +0100

    Before Ale

---
+++ src/hooks/useStake.tsx
@@ -0,0 +1,44 @@
+import { useMoralis } from "react-moralis";
+import NFT from "../../artifacts/contracts/NFT.sol/NFT.json";
+import NFTMarket from "../../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+type props = {
+  marketId?: number;
+  price?: string;
+  callback: () => void;
+  errCallback: () => void;
+};
+
+const BuyNFT = () => {
+  const { Moralis, isWeb3Enabled } = useMoralis();
+  
+  const buy = async (props: props) => {
+    !isWeb3Enabled ? await Moralis.enableWeb3() : null;
+    const marketAddress = process.env.NEXT_PUBLIC_NFT_MARKET_ADDRESS;
+    const userAddress = await Moralis.account;
+    if (!marketAddress || !userAddress || !props.price) return;
+
+    const { marketId, price, callback, errCallback } = props;
+    try {
+      const listItem = {
+        contractAddress: marketAddress,
+        functionName: "performATransaction",
+        abi: NFTMarket.abi,
+        params: {
+          itemId: marketId,
+        },
+        msgValue: Moralis.Units.ETH(price),
+      };
+      const listTransaction: any = await Moralis.executeFunction(listItem);
+      await listTransaction.wait();
+
+      callback();
+    } catch (err) {
+      console.log(err);
+      errCallback();
+    }
+  };
+
+  return buy;
+};
+
+export default BuyNFT;
